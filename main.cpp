2df3e9dc474a6714f8f206f7e625e89e16fc7dc1
17glatzjo <glatzj.a1999@gmail.com>
2021-04-28 09:44:03 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,19 +9,37 @@
 
 int main()
 {
-	Clock c1, c2;											//Variables
+	Clock c1, c2;												//Variables
 	bool checker;
 	
-	std::cout << "Enter First Time: ";						//Inputs
-	std::cin >> c1;
+	std::cout << "Enter First Time: ";							//Inputs
+	if (std::cin >> c1)
+	{
+	
+		std::cout << "\nYou have entered: " << c1 << "\n";
+	}
+	else
+	{
+		std::cout << "Invalid Time\n";
+		std::cin.clear();
+	}
 	std::cout << "Enter Second Time: ";
-	std::cin >> c2;
+	if (std::cin >> c2)
+	{
+		std::cout << "\nYou have entered: " << c2 << "\n";
+	}
+	else
+	{
+		std::cout << "Invalid Time \n";	
+		std::cinclear();
+	}
+	
 	c1.addmin(c1);
 	
-	std::cout << "Clock 1 says: " << c1;					//Outputs
+	std::cout << "Clock 1 says: " << c1;						//Outputs
 	std::cout << "Clock 2 says: " << c2;
 	
-	if (checker == true)											//Comparison results
+	if (checker == true)										//Comparison results
 	{
 		std::cout << "Clock 1 is later than Clock 2";
 	}
